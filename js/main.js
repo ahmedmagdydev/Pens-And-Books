28c7cb613bde08e31f5d3f983d1d762cb3aa0189
Ahmed Magdy <ahmed.magdy.dev@gmail.com>
2014-09-23 22:19:20 +0300

    m

--- js/main.js
+++ js/main.js
@@ -3,35 +3,55 @@ jQuery(document).ready(function($) {
 		leftBar		= $(".left-side-bar"),
 		store 		= $("article.store"),
 		profile		= $("article.profile"),
-		options 	=  {cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false};
+		options 	=  {cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false},
+		notif		= $(".notifications");
 
 		 // jqueryUi
 	$("#accordion").accordion();
 	$( ".select" ).selectmenu();
-	$( ".filter" ).menu({ 
-		position: { my: "center top", at: "right-40 top+45" } ,
-		icons: {  submenu: "ui-icon-carat-1-s" }
-	});
+	
 
 	 // divider
 	$("section.result").append('<div class="divider"></div>');
 	 
-
+	$("#search").parents(article).css('background-color', '#fff');
 	 	 // niceScroll
-	var myNiceScroll = function  (element) {
-	 	var maxHeight	= $(".side-bar").height()-20;
-	 	if (element.height() > maxHeight) {
-		element.height(maxHeight);
-		element.niceScroll(options);
-	 	};
-	};
+	// var myNiceScroll = function  (element) {
+	//  	var maxHeight	= $(".side-bar").height()-20;
+	//  	if (element.height() > maxHeight) {
+	// 	element.height(maxHeight);
+	// 	element.niceScroll(options);
+	//  	};
+	// };
 	 // myNiceScroll(article);
 	 // myNiceScroll(leftBar);
 	 // myNiceScroll(store);
 	 // myNiceScroll(profile);
+ 	$(".notifications").niceScroll(options);
+ 	$('.left-side-bar').scrollToFixed({
+	    marginTop: function() {
+	        var marginTop = $(window).height() - $('.left-side-bar').outerHeight(true) ;
+	        if (marginTop >= 0) return 0;
+	        return marginTop;
+	    }
+    });
 
+    
+	notif.hide();
+	$('html').click(function() {
+		notif.hide(100);
+		$(".flag a").removeClass('active');
+	});
+	notif.on('click', function(event) {
+		 event.stopPropagation();
+		/* Act on the event */
+	});
+ 	$(".flag").on('click', 'a', function(event) {
+ 		event.stopPropagation();
+ 		/* Act on the event */
+ 		$(this).toggleClass('active');
+ 		notif.toggle(100);
 
-	
-
+ 	});
 	 
 });
